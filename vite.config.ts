c81b23a2b21b64505e1d92e3e1337d70b80e5178
Ahmed <cmpunkthebest11@gmail.com>
2025-06-25 15:03:58 +0100

    vite build error4

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ["some-package-causing-issue"],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react";
-            if (id.includes("firebase")) return "firebase";
-            return "vendor";
-          }
-        },
-      },
-      // vite.config.ts
-    },
-  },
 });
