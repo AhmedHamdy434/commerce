de2c3bec6f9235d6ef5858120cc5529fc10e5827
Ahmed <cmpunkthebest11@gmail.com>
2025-06-25 11:44:15 +0100

    vite build error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ["some-package-causing-issue"],
+  },
   build: {
     rollupOptions: {
       output: {
@@ -15,6 +18,7 @@ export default defineConfig({
           }
         },
       },
+      // vite.config.ts
     },
   },
 });
